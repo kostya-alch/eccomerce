60d235241b32f725adae070963cbbd33ff9c1a8e
kostya-alch <kostafrompiter@gmail.com>
2022-11-09 00:03:45 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <div>Здарова!</div>
+  <hello-world msg="say" />
+</template>
+
+<style scoped></style>
